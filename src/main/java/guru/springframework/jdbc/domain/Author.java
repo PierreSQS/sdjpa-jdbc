20ba508381f3489e42ae29cf5e748090dc30956d
PierreSQS <nomo_messina@web.de>
2022-07-15 23:35:45 +0200

    Sect9_Assn5-Create DB-INC: (Step3 of Assn5) added entities and repos

---
+++ src/main/java/guru/springframework/jdbc/domain/Author.java
@@ -0,0 +1,47 @@
+package guru.springframework.jdbc.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
