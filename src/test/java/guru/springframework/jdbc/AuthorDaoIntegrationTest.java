f54becfbc379f7abc45afabb0de7edde972f3e60
PierreSQS <nomo_messina@web.de>
2022-07-16 15:25:02 +0200

    Sect9_Chap63-Create Author DAO: optimization of the previous commit(9bde4a18)

--- src/test/java/guru/springframework/jdbc/AuthorDaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/AuthorDaoIntegrationTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.jdbc;
 
 import guru.springframework.jdbc.dao.AuthorDao;
+import guru.springframework.jdbc.dao.AuthorDaoImpl;
 import guru.springframework.jdbc.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
@@ -13,7 +15,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("local")
-@SpringBootTest
+@DataJpaTest
+@Import(AuthorDaoImpl.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AuthorDaoIntegrationTest {
 
