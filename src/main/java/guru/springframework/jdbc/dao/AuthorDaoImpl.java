7ebeb1d449ddf1ad71d8d178b33a99c3f11b77b3
PierreSQS <nomo_messina@web.de>
2022-07-16 20:25:43 +0200

    Sect9_Chap68-Refactoring Duplicated code: related code

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -9,6 +9,9 @@ import java.util.Optional;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao{
+    public static final String SELECT_AUTHOR_BY_ID = "SELECT * FROM AUTHOR where id = ?";
+    public static final String SELECT_AUTHOR_BY_NAME = "SELECT * FROM AUTHOR where first_name = ? AND last_name = ?";
+
     private final DataSource dataSource;
 
     public AuthorDaoImpl(DataSource dataSource) {
@@ -17,41 +20,34 @@ public class AuthorDaoImpl implements AuthorDao{
 
     @Override
     public Optional<Author> findAuthorById(Long id) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement ps = connection.prepareStatement("SELECT * FROM AUTHOR where id = ?"))
-        {
-
-            ps.setLong(1,id);
-            ResultSet resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                Author foundAuthor = new Author();
-                foundAuthor.setId(id);
-                foundAuthor.setFirstName(resultSet.getString("first_name"));
-                foundAuthor.setLastName(resultSet.getString("last_name"));
-
-                return Optional.of(foundAuthor);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return Optional.empty();
+        return findAuthor(id, null,null, SELECT_AUTHOR_BY_ID);
     }
 
     @Override
     public Optional<Author> findAuthorByName(String firstName, String lastName) {
+        return findAuthor(null,firstName,lastName,SELECT_AUTHOR_BY_NAME);
+    }
+
+    private Optional<Author> findAuthor(Long id, String firstName, String lastName, String sqlCmd) {
         try (
                 Connection connection = dataSource.getConnection();
-                PreparedStatement ps = connection.prepareStatement("SELECT * FROM AUTHOR where first_name = ? AND last_name = ?"))
+                PreparedStatement ps = connection.prepareStatement(sqlCmd))
         {
 
-            ps.setString(1,firstName);
-            ps.setString(2,lastName);
+            Author foundAuthor = new Author();
+
+            if (id != null) {
+                ps.setLong(1,id);
+            } else {
+                ps.setString(1, firstName);
+                ps.setString(2, lastName);
+            }
+
             ResultSet resultSet = ps.executeQuery();
 
             if (resultSet.next()) {
-                Author foundAuthor = new Author();
+
+                foundAuthor.setId(resultSet.getLong("id"));
                 foundAuthor.setFirstName(resultSet.getString("first_name"));
                 foundAuthor.setLastName(resultSet.getString("last_name"));
 
